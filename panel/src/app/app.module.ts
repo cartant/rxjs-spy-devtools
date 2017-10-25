83b009d932a4d54fd736b1498fceea2bfa5fd89a
Nicholas Jamieson <nicholas@cartant.com>
2017-10-25 18:42:00 +1000

    chore(panel): Add material module, etc.

--- panel/src/app/app.module.ts
+++ panel/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
