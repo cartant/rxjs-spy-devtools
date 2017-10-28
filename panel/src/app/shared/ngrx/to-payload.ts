947fc94adb25e38cdefff69208a0522b0e7609d5
Nicholas Jamieson <nicholas@cartant.com>
2017-10-28 13:29:30 +1000

    chore(panel): Remove whitespace.

--- panel/src/app/shared/ngrx/to-payload.ts
+++ panel/src/app/shared/ngrx/to-payload.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
 import { ActionWithPayload } from './action-with-payload';
 
-export function toPayload<T>(
-): (source: Observable<ActionWithPayload<T>>) => Observable<T> {
+export function toPayload<T>(): (source: Observable<ActionWithPayload<T>>) => Observable<T> {
   return map(action => action.payload);
 }
