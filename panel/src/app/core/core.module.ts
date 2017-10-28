986612e08c45b311a23e2faeca1f8cb61af7cda0
Nicholas Jamieson <nicholas@cartant.com>
2017-10-28 14:47:34 +1000

    chore(panel): Add store and effects.

--- panel/src/app/core/core.module.ts
+++ panel/src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '@env/environment';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { ChromeMockService, ChromeService } from './chrome';
 
 const service = ChromeService;
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([])
   ],
   declarations: [],
   providers: [service]
