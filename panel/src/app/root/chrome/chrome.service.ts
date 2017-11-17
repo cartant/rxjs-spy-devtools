cecceeec0ec98a354f79b38e782458c13d26e3d5
Nicholas Jamieson <nicholas@cartant.com>
2017-11-17 13:31:09 +1000

    feat(panel): Add request method.

--- panel/src/app/root/chrome/chrome.service.ts
+++ panel/src/app/root/chrome/chrome.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NgZone, Provider } from '@angular/core';
-import { PANEL_BACKGROUND_CONNECT, PANEL_BACKGROUND_INIT } from '@devtools/constants';
-import { Post } from '@devtools/interfaces';
+import { PANEL_BACKGROUND_CONNECT, PANEL_BACKGROUND_INIT, PANEL_MESSAGE } from '@devtools/constants';
+import { Message, Post } from '@devtools/interfaces';
 import { Observable } from 'rxjs/Observable';
 import { empty } from 'rxjs/observable/empty';
 import { fromEventPattern } from 'rxjs/observable/fromEventPattern';
@@ -19,7 +19,9 @@ export function createChromeService(ngZone: NgZone): ChromeMockService | ChromeS
 @Injectable()
 export class ChromeService {
 
+  private static _posts = 0;
   public posts: Observable<Post>;
+  private _backgroundConnection: chrome.runtime.Port;
 
   public static forRoot(): Provider {
     return { deps: [NgZone], provide: ChromeService, useFactory: createChromeService };
@@ -28,15 +30,21 @@ export class ChromeService {
   constructor(ngZone: NgZone) {
 
     const tabId = chrome.devtools.inspectedWindow.tabId;
-    const backgroundConnection = chrome.runtime.connect({ name: PANEL_BACKGROUND_CONNECT });
-    backgroundConnection.postMessage({ postType: PANEL_BACKGROUND_INIT, tabId });
+    this._backgroundConnection = chrome.runtime.connect({ name: PANEL_BACKGROUND_CONNECT });
+    this._backgroundConnection.postMessage({ postType: PANEL_BACKGROUND_INIT, tabId });
 
     this.posts = fromEventPattern<Post>(
-      handler => backgroundConnection.onMessage.addListener(handler as MessageListener),
-      handler => backgroundConnection.onMessage.removeListener(handler as MessageListener)
+      handler => this._backgroundConnection.onMessage.addListener(handler as MessageListener),
+      handler => this._backgroundConnection.onMessage.removeListener(handler as MessageListener)
     ).pipe(
       observeOn(enterZone(ngZone)),
       share()
     );
+  }
+
+  post(message: Message): Post {
+    const post = { ...message, postId: (++ChromeService._posts).toString(), postType: PANEL_MESSAGE };
+    this._backgroundConnection.postMessage(post);
+    return post;
   }
 }
--- panel/src/app/root/spy/spy.service.ts
+++ panel/src/app/root/spy/spy.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { CONTENT_MESSAGE } from '@devtools/constants';
-import { isNotification, isPostRequest } from '@devtools/guards';
-import { Notification, Post, Request } from '@devtools/interfaces';
+import { isNotification, isPostRequest, isPostResponse } from '@devtools/guards';
+import { Notification, Post, Request, Response } from '@devtools/interfaces';
 import { Observable } from 'rxjs/Observable';
 import { filter } from 'rxjs/operators/filter';
+import { take } from 'rxjs/operators/take';
 import { share } from 'rxjs/operators/share';
 import { ChromeService } from '../chrome';
 
@@ -13,21 +14,35 @@ export class SpyService {
   public notifications: Observable<Notification>;
   public posts: Observable<Post>;
   public requests: Observable<Post & Request>;
+  public responses: Observable<Post & Response>;
 
-  constructor(chromeService: ChromeService) {
-    this.notifications = chromeService.posts.pipe(
+  constructor(private _chromeService: ChromeService) {
+    this.notifications = _chromeService.posts.pipe(
       filter(post => post.postType === CONTENT_MESSAGE),
       filter(isNotification),
       share()
     );
-    this.posts = chromeService.posts.pipe(
+    this.posts = _chromeService.posts.pipe(
       filter(post => post.postType === CONTENT_MESSAGE),
       share()
     );
-    this.requests = chromeService.posts.pipe(
+    this.requests = _chromeService.posts.pipe(
       filter(post => post.postType === CONTENT_MESSAGE),
       filter(isPostRequest),
       share()
+    );
+    this.responses = _chromeService.posts.pipe(
+      filter(post => post.postType === CONTENT_MESSAGE),
+      filter(isPostResponse),
+      share()
+    );
+  }
+
+  request(request: Request): Observable<Post & Response> {
+    const { postId } = this._chromeService.post(request);
+    return this.responses.pipe(
+      filter(response => response.request.postId === postId),
+      take(1)
     );
   }
 }
