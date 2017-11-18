a68fe3adba4ef9da20c6a545f92e5dd39f1b83b8
Nicholas Jamieson <nicholas@cartant.com>
2017-11-18 22:24:50 +1000

    refactor(panel): Simplify action helpers.

--- panel/src/app/root/spy/spy.actions.ts
+++ panel/src/app/root/spy/spy.actions.ts
@@ -5,10 +5,6 @@ function errorBase<T>(actionCtor: Ctor<T>) {
   return base(class { constructor(public error: string, public request: T) {} });
 }
 
-function deckBase() {
-  return base(class { constructor(public pluginId: string, public deck: string) {} });
-}
-
 function pluginFulfilledBase() {
   return base(class { constructor(public id: string, public pluginId: string) {} });
 }
@@ -24,24 +20,24 @@ function pluginRequestedBase() {
 export const Connect = action('[Spy] CONNECT');
 export const Disconnect = action('[Spy] DISCONNECT');
 
-export const DeckClear = action('[Spy] DECK_CLEAR', deckBase());
-export const DeckClearFulfilled = action('[Spy] DECK_CLEAR_FULFILLED', deckBase());
+export const DeckClear = action('[Spy] DECK_CLEAR', pluginIdBase());
+export const DeckClearFulfilled = action('[Spy] DECK_CLEAR_FULFILLED', pluginIdBase());
 export const DeckClearRejected = action('[Spy] DECK_CLEAR_REJECTED', errorBase(DeckClear));
 
-export const DeckPause = action('[Spy] DECK_PAUSE', deckBase());
-export const DeckPauseFulfilled = action('[Spy] DECK_PAUSE_FULFILLED', deckBase());
+export const DeckPause = action('[Spy] DECK_PAUSE', pluginIdBase());
+export const DeckPauseFulfilled = action('[Spy] DECK_PAUSE_FULFILLED', pluginIdBase());
 export const DeckPauseRejected = action('[Spy] DECK_PAUSE_REJECTED', errorBase(DeckPause));
 
-export const DeckResume = action('[Spy] DECK_RESUME', deckBase());
-export const DeckResumeFulfilled = action('[Spy] DECK_RESUME_FULFILLED', deckBase());
+export const DeckResume = action('[Spy] DECK_RESUME', pluginIdBase());
+export const DeckResumeFulfilled = action('[Spy] DECK_RESUME_FULFILLED', pluginIdBase());
 export const DeckResumeRejected = action('[Spy] DECK_RESUME_REJECTED', errorBase(DeckResume));
 
-export const DeckSkip = action('[Spy] DECK_SKIP', deckBase());
-export const DeckSkipFulfilled = action('[Spy] DECK_SKIP_FULFILLED', deckBase());
+export const DeckSkip = action('[Spy] DECK_SKIP', pluginIdBase());
+export const DeckSkipFulfilled = action('[Spy] DECK_SKIP_FULFILLED', pluginIdBase());
 export const DeckSkipRejected = action('[Spy] DECK_SKIP_REJECTED', errorBase(DeckSkip));
 
-export const DeckStep = action('[Spy] DECK_STEP', deckBase());
-export const DeckStepFulfilled = action('[Spy] DECK_STEP_FULFILLED', deckBase());
+export const DeckStep = action('[Spy] DECK_STEP', pluginIdBase());
+export const DeckStepFulfilled = action('[Spy] DECK_STEP_FULFILLED', pluginIdBase());
 export const DeckStepRejected = action('[Spy] DECK_STEP_REJECTED', errorBase(DeckStep));
 
 export const Log = action('[Spy] LOG', pluginRequestedBase());
