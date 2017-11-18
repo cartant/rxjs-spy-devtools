c214abcc0a81c6fe3174f8a0901775117df39382
Nicholas Jamieson <nicholas@cartant.com>
2017-11-18 15:49:55 +1000

    chore(panel): Add deck actions.

--- panel/src/app/root/spy/spy.actions.ts
+++ panel/src/app/root/spy/spy.actions.ts
@@ -5,6 +5,10 @@ function errorBase<T>(actionCtor: Ctor<T>) {
   return base(class { constructor(public error: string, public request: T) {} });
 }
 
+function deckBase() {
+  return base(class { constructor(public pluginId: string, public deck: string) {} });
+}
+
 function matchBase() {
   return base(class { constructor(public match: string) {} });
 }
@@ -15,6 +19,26 @@ function pluginIdBase() {
 
 export const Connect = action('[Spy] CONNECT');
 export const Disconnect = action('[Spy] DISCONNECT');
+
+export const DeckClear = action('[Spy] DECK_CLEAR', deckBase());
+export const DeckClearFulfilled = action('[Spy] DECK_CLEAR_FULFILLED', deckBase());
+export const DeckClearRejected = action('[Spy] DECK_CLEAR_REJECTED', errorBase(DeckClear));
+
+export const DeckPause = action('[Spy] DECK_PAUSE', deckBase());
+export const DeckPauseFulfilled = action('[Spy] DECK_PAUSE_FULFILLED', deckBase());
+export const DeckPauseRejected = action('[Spy] DECK_PAUSE_REJECTED', errorBase(DeckPause));
+
+export const DeckResume = action('[Spy] DECK_RESUME', deckBase());
+export const DeckResumeFulfilled = action('[Spy] DECK_RESUME_FULFILLED', deckBase());
+export const DeckResumeRejected = action('[Spy] DECK_RESUME_REJECTED', errorBase(DeckResume));
+
+export const DeckSkip = action('[Spy] DECK_SKIP', deckBase());
+export const DeckSkipFulfilled = action('[Spy] DECK_SKIP_FULFILLED', deckBase());
+export const DeckSkipRejected = action('[Spy] DECK_SKIP_REJECTED', errorBase(DeckSkip));
+
+export const DeckStep = action('[Spy] DECK_STEP', deckBase());
+export const DeckStepFulfilled = action('[Spy] DECK_STEP_FULFILLED', deckBase());
+export const DeckStepRejected = action('[Spy] DECK_STEP_REJECTED', errorBase(DeckStep));
 
 export const Log = action('[Spy] LOG', matchBase());
 export const LogFulfilled = action('[Spy] LOG_FULFILLED', pluginIdBase());
