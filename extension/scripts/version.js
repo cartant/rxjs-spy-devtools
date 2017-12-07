d66dc3711e0de79341bb00f1c5b8390d42310527
Nicholas Jamieson <nicholas@cartant.com>
2017-12-07 14:03:16 +1000

    chore(package): Add a version script.

---
+++ extension/scripts/version.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const fs = require("fs");
+const [,, version] = process.argv;
+
+if (!/^\d+\.\d+\.\d+$/.test(version)) {
+    throw new Error(`${version} is not a valid Chrome manifest version: https://developer.chrome.com/extensions/manifest/version`);
+}
+
+const files = [
+    "./assets/manifest.json",
+    "./package.json",
+    "../panel/package.json"
+];
+files.forEach(file => {
+    const content = fs.readFileSync(file).toString();
+    fs.writeFileSync(file, content.replace(/"version"\s*:\s*"\d+\.\d+\.\d+"/, `"version": "${version}"`));
+});
