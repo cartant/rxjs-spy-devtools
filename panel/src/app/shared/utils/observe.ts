2eadf3e8db1d9e9982b849c9ab9748afd0b9d9bc
Nicholas Jamieson <nicholas@cartant.com>
2018-05-19 13:11:57 +1000

    chore(*): Remove observe.
    Use the observe in rxjs-marbles, instead.

--- panel/src/app/shared/utils/observe.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-
-export function observe<T>(observableTest: () => Observable<T>): (callbackTest: (error?: Error) => void) => void {
-
-    return (callbackTest: (error?: Error) => void) => observableTest().subscribe(undefined, callbackTest, callbackTest);
-}
